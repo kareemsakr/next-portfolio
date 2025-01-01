12b60097bb66afe6e3a53485c3758c3416897303
Kareem Sakr <kareem.h.sakr@gmail.com>
2025-01-01 17:09:52 -0500

    updated the next config to work with github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // Generates static HTML/CSS/JS files
+  images: {
+    unoptimized: true, // Required for static export
+  },
+  basePath: process.env.NODE_ENV === "production" ? "/next-portfolio" : "", // Important for GitHub Pages
 };
 
 export default nextConfig;
